ffe9a5825067d98ac435f4a7fd6e75337e3b0f7e
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-05-07 15:43:45 -0700

    finished user prompt

--- script.js
+++ script.js
@@ -3,53 +3,71 @@ var newPassword = document.querySelector("#password");
 var generateButton = document.querySelector("#generate");
 var copyButton = document.querySelector("#copy");
 
-var randomCharacter = "";
 
-var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", ">", "<", "?", "{", "}", "[", "]", ".", ",", " " ];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", ">", "<", "?", "{", "}", "[", "]", ".", ",", " "];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var characeterSet = specialCharacter + number + lowerCase + upperCase;
-
-
+var randomCharacter = "";
+// var characeterSet = specialCharacter + number + lowerCase + upperCase;
 var randomText = "";
-
-
-
-   var ranNum = Math.floor(Math.random() * 128) + 8;
-
-
+// var ranNum = Math.floor(Math.random() * 128) + 8;
 
 
 
 function randomGenerator() {
+    randomText = "";
 
+    var pwLength = prompt("how long do you want your password? (Please type number only)")
+    if (isNaN(pwLength) === true) {
+        length = prompt("Numbers only");
+        console.log(pwLength);
 
+    } else {
+        console.log(pwLength);
+        var userAnswer = prompt("Choose either Number, Alphabet, or Special Character to create random password")
+        userAnswer = userAnswer.toLowerCase();
 
-    for ( var i = 0; i < ranNum; i++)
-    { 
-        randomCharacter = characeterSet[Math.floor(Math.random()*characeterSet.length)];
-        randomText += randomCharacter;
-    }
+        console.log(userAnswer);
+
+        if (userAnswer == "number" || userAnswer == "numbers") {
+            for (var i = 0; i < pwLength; i++) {
+                randomCharacter = number[Math.floor(Math.random() * number.length)];
+                randomText += randomCharacter;
+            }
+        }
+
+        else if (userAnswer == "alphabet" || userAnswer == "alphabets") {
+            for (var i = 0; i < pwLength; i++) {
+                randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
+                randomText += randomCharacter;
+            }
+        }
+
+        else if (userAnswer == "special case" || userAnswer == "special case") {
+            for (var i = 0; i < pwLength; i++) {
+                randomCharacter = specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
+                randomText += randomCharacter;
+            }
+        }
+
+        else {
+             userAnswer = prompt("Please type out again (Number, Alphabet, or Special Case)")
+            userAnswer = userAnswer.toLowerCase();       
         
+        }
 
         newPassword.innerText = randomText;
     }
-
-
-function copyNewPW() {
-
-    newPassword.select();
-    newPassword.setSelectionRange(0, 99999);
-
-    document.execCommand("copy");
-   
-
 }
 
+function copyNewPW() {
+    newPassword.select();
+    newPassword.setSelectionRange(0, 99999);
+    document.execCommand("copy");
 
-
+}
 
 generateButton.addEventListener("click", randomGenerator);
 copyButton.addEventListener("click", copyNewPW);
@@ -57,11 +75,8 @@ copyButton.addEventListener("click", copyNewPW);
 
 
 
-// for ( var i = 0; i < 8; i++){
 
-//     var random = Math.floor(Math.random() * 9);
-  
-//      randomNumber = random + randomNumber; 
-//     }; 
 
-// newPassword.innerText = randomNumber;
+
+
+
